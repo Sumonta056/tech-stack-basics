072dc5bd748245d55d58fe9b6eaa750c9df722b7
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-06-18 21:13:08 +0600

    feat: react hook form with zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 import Like from "./components/like-unlike/Like";
+import Form from "./react-hook-form-zod/Form";
 import DataFetching from "./tanstack-query/dataFetching";
 import { DataPushing } from "./tanstack-query/dataPushing";
 import TodoList from "./zustand/TodoList";
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/2" element={<DataFetching />} />
         <Route path="/3" element={<DataPushing />} />
         <Route path="/4" element={<TodoList />} />
+        <Route path="/5" element={<Form />} />
         <Route path="*" element={<div>Not Found</div>} />
         {/* <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
