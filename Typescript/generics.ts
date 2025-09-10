1d23a2832cfd777b98dfdf2cb888312f53f77c41
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-09-10 08:46:11 +0600

    feat: generic real world example

--- Typescript/generics.ts
+++ Typescript/generics.ts
@@ -1,3 +1,10 @@
+//----------------------------
+// Generic Types
+
+type Box<T> = {
+  content: T;
+};
+
 function wrapInBox<T>(value: T): { returnType: T } {
   return { returnType: value };
 }
@@ -6,12 +13,18 @@ const stringBox = wrapInBox("hello"); // { content: string }
 const numBox = wrapInBox(42); // { content: number }
 const chaosBox = wrapInBox({ wow: true }); // { content: { wow: boolean } }
 
+//----------------------------
+// Generic Functions
+
 function firstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
 const firstNum = firstElement([1, 2, 3]); // number
 const firstStr = firstElement(["a", "b", "c"]); // string
+
+//----------------------------
+// Generic Interfaces
 
 interface DataStore<T> {
   content: T;
@@ -22,3 +35,35 @@ function wrapInBox2<T>(value: T): DataStore<T> {
 }
 
 const boolBox = wrapInBox2(true); // Box<boolean>
+
+//----------------------------
+// Real World Example
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface User {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+type ApiResponse<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+};
+
+const todoResponse: ApiResponse<Todo[]> = {
+  data: [{ id: 1, text: "Learn TypeScript", completed: false }],
+  loading: false,
+  error: null,
+};
+const userResponse: ApiResponse<User> = {
+  data: { name: "Alex", email: "alex@example.com", phone: "123-456-7890" },
+  loading: true,
+  error: null,
+};
